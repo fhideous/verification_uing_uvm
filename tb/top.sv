e7767a79a3bfe6240fbf11e622408a33fce4f28f
Кулешов Владислав <kul.leshiy@yandex.ru>
2022-01-21 17:57:44 +0300

    Feat(top.sv) пункт 4 лабника
    Подключил два новых класса, в инишале указ запуск теста, имя
    которого определяется в запускающем скрипте run.f

--- tb/top.sv
+++ tb/top.sv
@@ -5,37 +5,11 @@ module top;
   `include "uvm_macros.svh"
   import yapp_pkg::*;
 
-  yapp_packet packet;
-  yapp_packet copy_packet;
-  yapp_packet clone_packet;
+  `include "router_tb.sv"
+  `include "router_test_lib.sv"
 
-  initial begin
-
-  copy_packet = new("copy_packet");
-
-  for (int i=0; i<3; i++) begin
-    packet = new( $sformatf("packet_%0d", i) );
-    packet.randomize();
-    packet.print(uvm_default_table_printer);
-  end
-
-  $display("\nCOPY");
-  copy_packet.copy(packet);
-  copy_packet.print();
-
- 
-  $display("CLONE");
-  copy_packet.compare(packet);
-  copy_packet.randomize();
-  copy_packet.compare(packet);
-  
-  $display("CLONE");
-  $cast(clone_packet, packet.clone()); 
-  clone_packet.print();
-
-
-
-end
+  initial
+    run_test();
 
 endmodule : top
 
