b0b65d1d7c6954c9c0938d9b8fcfada5cf54fbfc
Кулешов Владислав <kul.leshiy@yandex.ru>
2022-01-25 18:57:22 +0300

    Feat(router_tb) Добавил вызов класса env

--- lab03/tb/router_tb.sv
+++ lab03/tb/router_tb.sv
@@ -3,14 +3,17 @@ class router_tb extends uvm_env;
 
   `uvm_component_utils(router_tb)
 
+  yapp_env yapp;
+
   function new(string name, uvm_component parent);
     super.new(name, parent);
   endfunction : new
 
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-  
+    yapp = new("yapp", this);
     `uvm_info(get_type_name(), "Build Phase of the testbench is being exexuted", UVM_HIGH)
 
   endfunction : build_phase
+
 endclass : router_tb
