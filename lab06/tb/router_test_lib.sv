98276b412f62e099774a74de0ff9ee588a80790c
Кулешов Владислав <kul.leshiy@yandex.ru>
2022-02-04 17:57:56 +0300

    Fix(router_test_lib.sv)  change run_phase declaration
    Was needed using task instead function

--- lab06/tb/router_test_lib.sv
+++ lab06/tb/router_test_lib.sv
@@ -18,15 +18,15 @@ class base_test extends uvm_test;
 
 //The drain time allows packets to pass through the router before simulation ends
  // Why i need it?... idk 
-  function void run_phase(uvm_phase phase);
+  task run_phase(uvm_phase phase);
     uvm_objection obj = phase.get_objection();
     obj.set_drain_time(this, 200ns);
-  endfunction : run_phase
+  endtask : run_phase
 
   //This will help debug configuration errors by reporting any unmatched settings.
   function void check_phase(uvm_phase phase);
     check_config_usage();
-  endfunction
+  endfunction : check_phase
 
   function void end_of_elaboration_phase(uvm_phase phase);
     uvm_top.print_topology();    
