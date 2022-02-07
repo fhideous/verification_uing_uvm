f963da1b8211c2027c18f0fc252d66840d5e8e63
Кулешов Владислав <kul.leshiy@yandex.ru>
2022-02-07 17:25:16 +0300

    Feat(hw_top.sv) Add interface instantiations
    Connect channel interfacrs and hbus to the DUT

--- lab07/tb/hw_top.sv
+++ lab07/tb/hw_top.sv
@@ -7,48 +7,61 @@ module hw_top;
   logic         reset;
 
   // YAPP Interface to the DUT
-  yapp_if in0(clock, reset);
+  yapp_if in0(
+    clock, 
+    reset
+  );
 
   // CLKGEN module generates clock
   clkgen clkgen (
-    .clock(clock),
-    .run_clock(1'b1),
-    .clock_period(32'd10)
+    .clock          ,
+    .run_clock      ,
+    .clock_period
   );
 
-  yapp_router dut(
-  .reset(reset),
-  .clock(clock),
-  .error(),
-  // YAPP interface
-  .in_data(in0.in_data),
-  .in_data_vld(in0.in_data_vld),
-  .in_suspend(in0.in_suspend),
-  // Output Channels
-  //Channel 0
-  .data_0(),
-  .data_vld_0(),
-  .suspend_0(1'b0),
-  //Channel 1
-  .data_1(),
-  .data_vld_1(),
-  .suspend_1(1'b0),
-  //Channel 2
-  .data_2(),
-  .data_vld_2(),
-  .suspend_2(1'b0),
-  // HBUS Interface 
-  .haddr(),
-  .hdata(),
-  .hen(),
-  .hwr_rd());
+  clock_and_reset_if clk_reset_if (
+    .clock          ,
+    .reset          ,
+    .run_clock      ,
+    .clock_perion
+  );
 
-  initial begin
-    reset <= 1'b0;
-    @(negedge clock)
-      #1 reset <= 1'b1;
-    @(negedge clock)
-      #1 reset <= 1'b0;
-  end
+  hbus_if hbus_if (
+      clock,
+      reset
+  );
+
+  channel_if ch0(clock, reset);
+  channel_if ch1(clock, reset);
+  channel_if ch2(clock, reset);
+
+  yapp_router dut(
+    .reset      (   reset             ),
+    .clock      (   clock             ),
+    .error      (                     ),
+    // YAPP interface
+    .in_data    (   in0.in_data       ),
+    .in_data_vld(   in0.in_data_vld   ),
+    .in_suspend (   in0.in_suspend    ),
+    // Output Channels
+    //Channel 0
+    .data_0     (   ch0.data          ),
+    .data_vld_0 (   ch0.data_vld      ),
+    .suspend_0  (   ch0.suspend       ),
+    //Channel 1
+    .data_1     (   ch1.data          ),
+    .data_vld_1 (   ch1.data_vld      ),
+    .suspend_1  (   ch1.suspend       ),
+    //Channel 2
+    .data_2     (   ch2.data          ),
+    .data_vld_2 (   ch2.data_vld      ),
+    .suspend_2  (   ch2.suspend       ),
+    // HBUS Interface 
+    .haddr      (   hbus_if.haddr     ),
+    .hdata      (   hbus_if.hdata_w   ),
+    .hen        (   hbus_if.hen       ),
+    .hwr_rd     (   hbus_if.hwr_rd    )
+  
+  );
 
 endmodule
