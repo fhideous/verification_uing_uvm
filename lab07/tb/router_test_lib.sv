493513358f9d7b3d116134da1a7b32087e693a22
Кулешов Владислав <kul.leshiy@yandex.ru>
2022-02-08 16:37:36 +0300

    Feat(router_test_lib.sv) Add new class to execute new UVCs
    Execute yapp_uvc with yapp_012_seq and some other new
    uvcs with their sequence. Delete all previous test besides base_test
    Style fix for tb_top
    
     "default phase sequence for phase 'run'" and  packets will not sending.

--- lab07/tb/router_test_lib.sv
+++ lab07/tb/router_test_lib.sv
@@ -38,108 +38,34 @@ endclass : base_test
 // End of Based class 
 ///===================
 
-class short_packet_test extends base_test;
 
-  `uvm_component_utils(short_packet_test)
-
-  function new(string name, uvm_component parent);
-      super.new(name, parent);
-  endfunction : new
-
-  function void build_phase(uvm_phase phase);
-    yapp_packet::type_id::set_type_override(short_yapp_packet::get_type()); 
-    super.build_phase(phase);
-    uvm_config_wrapper::set(this, "root_tb.yapp.tx_agent.sequencer.run_phase",
-                                "default_sequence",
-                                yapp_5_packets::get_type());
-  endfunction : build_phase
-
-endclass : short_packet_test
-
-//-------------------------------------
-// TEST. Driver and srquencer are disable
-//-------------------------------------
-
-class set_config_test extends base_test;
-
-  `uvm_component_utils(set_config_test)
-
-  function new(string name, uvm_component parent);  
-    super.new(name, parent);
-  endfunction : new
-
-  function void build_phase(uvm_phase phase);
-    uvm_config_int::set(this, "root_tb.yapp.tx_agent", "is_active", UVM_PASSIVE);
-    uvm_config_wrapper::set(this, "root_tb.yapp.tx_agent.sequencer.run_phase",
-                                "default_sequence",
-                                yapp_5_packets::get_type());
-    super.build_phase(phase);
-  endfunction : build_phase
-
-endclass : set_config_test
-
-//-------------------------------------
-//  TEST. Call short packet with yapp_incr_payload_seq
-//-------------------------------------
-
-class incr_payload_test extends base_test;
-
-  `uvm_component_utils(incr_payload_test)
-
-  function new(string name, uvm_component parent);  
-    super.new(name, parent);
-  endfunction : new
-
-  function void build_phase(uvm_phase phase);
-    yapp_packet::type_id::set_type_override(short_yapp_packet::get_type());
-    uvm_config_wrapper::set(this, "root_tb.yapp.tx_agent.sequencer.run_phase",
-                                "default_sequence",
-                                yapp_incr_payload_seq::get_type());
-    super.build_phase(phase);
-  endfunction : build_phase
-
-endclass : incr_payload_test
-
-//-------------------------------------
-//  TEST. Call sequence which executes previous sequences
-//-------------------------------------
-
-class exhaustive_seq_test extends base_test;
-
-  `uvm_component_utils(exhaustive_seq_test)
-
-  function new(string name, uvm_component parent);  
-    super.new(name, parent);
-  endfunction : new
-
-  function void build_phase(uvm_phase phase);
-    yapp_packet::type_id::set_type_override(short_yapp_packet::get_type());
-    uvm_config_wrapper::set(this, "root_tb.yapp.tx_agent.sequencer.run_phase",
-                                "default_sequence",
-                                yapp_exhaustive_seq::get_type());
-    super.build_phase(phase);
-  endfunction : build_phase
-
-endclass : exhaustive_seq_test
 
 //--------------
-// Testing sending packets to all three output. Call yapp_012_seq 
+// Execute yapp_uvc with yapp_012_seq and some other new uvcs with their sequence 
 //---------------
 
-class all_outputs extends base_test;
+class simple_test extends base_test;
 
-  `uvm_component_utils(all_outputs)
+  `uvm_component_utils(simple_test)
 
   function new(string name, uvm_component parent);  
     super.new(name, parent);
   endfunction : new
 
   function void build_phase(uvm_phase phase);
+    // set_type_override_by_type(yapp_packet::get_type(), short_yapp_packet::get_type());
     yapp_packet::type_id::set_type_override(short_yapp_packet::get_type());
     uvm_config_wrapper::set(this, "root_tb.yapp.tx_agent.sequencer.run_phase",
                                 "default_sequence",
                                 yapp_012_seq::get_type());
-    super.build_phase(phase);
+    uvm_config_wrapper::set(this, "root_tb.clk_reset.agent.sequencer.run_phase",
+                            "default_sequence",
+                            clk10_rst5_seq::get_type());
+    // execute uvc for all three channels
+    uvm_config_wrapper::set(this, "root_tb.ch?.rx_agent.sequencer.run_phase",
+                            "default_sequence",
+                            channel_rx_resp_seq::get_type());  
+    super.build_phase(phase);  
   endfunction : build_phase
 
-endclass : all_outputs
+endclass : simple_test
--- lab07/tb/tb_top.sv
+++ lab07/tb/tb_top.sv
@@ -15,10 +15,10 @@ module tb_top;
   initial begin 
     yapp_vif_config::set            (null,  "*.root_tb.yapp.tx_agent.*",  "vif", hw_top.in0             );
     hbus_vif_config::set            (null,  "*.root_tb.hbus.*" ,          "vif", hw_top.hbus_if         );
-    channel_vif_config::set         (null,  "*.root_tb.ch0.*",          "vif", hw_top.ch0             );
-    channel_vif_config::set         (null,  "*.root_tb.ch1.*",          "vif", hw_top.ch1             );
-    channel_vif_config::set         (null,  "*.root_tb.ch2.*",          "vif", hw_top.ch2             );
-    clock_and_reset_vif_config::set (null,  "*.root_tb.clk_reset.*", "vif", hw_top.clk_reset_if   );
+    channel_vif_config::set         (null,  "*.root_tb.ch0.*",            "vif", hw_top.ch0             );
+    channel_vif_config::set         (null,  "*.root_tb.ch1.*",            "vif", hw_top.ch1             );
+    channel_vif_config::set         (null,  "*.root_tb.ch2.*",            "vif", hw_top.ch2             );
+    clock_and_reset_vif_config::set (null,  "*.root_tb.clk_reset.*",      "vif", hw_top.clk_reset_if    );
     
     run_test();
   end 
